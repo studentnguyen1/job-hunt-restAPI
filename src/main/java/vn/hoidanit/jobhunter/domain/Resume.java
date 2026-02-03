415ffb680abed9e36e5b236fc703b4abbcceaeda
“nguyenvailon” <nguyendeptrai812@gmail.com>
2026-02-03 16:07:20 +0700

    Update: Module Resume Entity

--- src/main/java/vn/hoidanit/jobhunter/domain/Resume.java
+++ src/main/java/vn/hoidanit/jobhunter/domain/Resume.java
@@ -1,75 +1,52 @@
 package vn.hoidanit.jobhunter.domain;
 
 import java.time.Instant;
-import java.util.List;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import vn.hoidanit.jobhunter.util.SecurityUtil;
-import vn.hoidanit.jobhunter.util.constant.LevelEnum;
+import vn.hoidanit.jobhunter.util.constant.ResumeStateEnum;
 
 @Entity
-@Table(name = "jobs")
+@Table(name = "resumes")
 @Getter
 @Setter
-
-public class Job {
+public class Resume {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    private String name;
-    private String location;
-    private double salary;
-    private int quantity;
+    private String email;
+
+    private String url;
 
     @Enumerated(EnumType.STRING)
-    private LevelEnum level;
-
-    @Column(columnDefinition = "MEDIUMTEXT")
-    private String description;
-
-    private Instant startDate;
-    private Instant endDate;
-    private boolean active;
+    private ResumeStateEnum state;
 
     private Instant createdAt;
-
     private Instant updatedAt;
+
     private String createdBy;
     private String updatedBy;
 
     @ManyToOne
-    @JoinColumn(name = "company_id")
-    private Company company;
+    @JoinColumn(name = "user_id")
+    private User user;
 
-    @ManyToMany(fetch = FetchType.LAZY)
-    @JsonIgnoreProperties(value = { "jobs" })
-    @JoinTable(name = "job_skill", joinColumns = @JoinColumn(name = "job_id"), inverseJoinColumns = @JoinColumn(name = "skill_id"))
-    private List<Skill> skills;
-
-    @OneToMany(mappedBy = "job", fetch = FetchType.LAZY)
-    @JsonIgnore
-    List<Resume> resumes;
+    @ManyToOne
+    @JoinColumn(name = "job_id")
+    private Job job;
 
     @PrePersist
     public void handleBeforeCreate() {
@@ -86,4 +63,5 @@ public class Job {
                 : "";
         this.updatedAt = Instant.now();
     }
+
 }
---
+++ src/main/java/vn/hoidanit/jobhunter/util/constant/ResumeStateEnum.java
@@ -0,0 +1,8 @@
+package vn.hoidanit.jobhunter.util.constant;
+
+public enum ResumeStateEnum {
+    PENDING,
+    REVIEWING,
+    APPROVED,
+    REJECTED
+}
