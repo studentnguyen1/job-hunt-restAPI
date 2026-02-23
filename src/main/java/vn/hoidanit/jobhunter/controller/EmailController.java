12633dce7447b615fb5027c327c8f7ec8f4e7176
“nguyenvailon” <nguyendeptrai812@gmail.com>
2026-02-24 00:51:24 +0700

    Update : Cron Job - schedule for module auto process

--- src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
+++ src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
@@ -1,5 +1,7 @@
 package vn.hoidanit.jobhunter.controller;
 
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,11 +24,14 @@ public class EmailController {
 
     @GetMapping("/email")
     @ApiMessage("Send simple email")
+    @Scheduled(cron = "*/5 * * * * *")
+    @Transactional
     public String getMethodName() {
         // this.emailService.sendSimpleEmail();
         // this.emailService.sendEmailSync("nguyengoby@gmail.com", "Test Email", "<h1>
         // <b>Hello from Spring Boot</b></h1>",
         // false, true);
+        System.out.println("Sending emails to subscribers...");
         this.subscriberService.sendSubscribersEmailJobs();
         return "Test";
     }
